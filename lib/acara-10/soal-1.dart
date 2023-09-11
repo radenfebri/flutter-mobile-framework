ed64e432aff90cfca30357e675a895b7d1abb6e5
Eka Febriansah <febriye12@gmail.com>
2023-09-11 19:21:36 +0700

    Update

--- lib/acara-10/soal-1.dart
+++ lib/acara-10/soal-1.dart
@@ -1,19 +1,27 @@
+// Mendefinisikan class Segitiga
 class Segitiga {
   double setengah, alas, tinggi;
 
+  // Konstruktor dengan parameter alas dan tinggi
   Segitiga(this.alas, this.tinggi) : setengah = 0.5;
 
+  // Metode untuk menghitung luas segitiga
   double hitungLuas() {
     return setengah * alas * tinggi;
   }
 }
 
 void main(List<String> args) {
+  // Menginisialisasi variabel alas dan tinggi
   double alas = 20.0;
   double tinggi = 30.0;
 
+  // Membuat objek Segitiga dengan menggunakan konstruktor
   Segitiga segitiga = Segitiga(alas, tinggi);
+  
+  // Menghitung luas segitiga
   double luasSegitiga = segitiga.hitungLuas();
 
+  // Mencetak hasil luas segitiga
   print("Luas Segitiga: $luasSegitiga");
 }
--- lib/acara-8/looping-while.dart
+++ lib/acara-8/looping-while.dart
@@ -2,16 +2,18 @@ void main() {
   print("LOOPING PERTAMA"); // Mencetak pesan "LOOPING PERTAMA" ke layar.
 
   int i = 1; // Inisialisasi variabel i dengan nilai 1.
-  while (i <= 10) { // Memulai perulangan while dengan kondisi i kurang dari atau sama dengan 10.
-    print("$i-i love her"); // Mencetak nilai i dan pesan "i love her".
+  while (i <= 10) {
+    // Memulai perulangan while dengan kondisi i kurang dari atau sama dengan 10.
+    print("$i - Saya suka sama kamu"); // Mencetak nilai i dan pesan "Saya suka sama kamu".
     i++; // Menambahkan nilai i sebesar 1.
   }
 
   print("LOOPING KEDUA"); // Mencetak pesan "LOOPING KEDUA" ke layar.
 
   int j = 10; // Inisialisasi variabel j dengan nilai 10.
-  while (j >= 1) { // Memulai perulangan while dengan kondisi j lebih besar dari atau sama dengan 1.
-    print("$j-she not love me"); // Mencetak nilai j dan pesan "she not love me".
+  while (j >= 1) {
+    // Memulai perulangan while dengan kondisi j lebih besar dari atau sama dengan 1.
+    print("$j - Saya tidak suka sama kamu"); // Mencetak nilai j dan pesan "Saya tidak suka sama kamu".
     j--; // Mengurangkan nilai j sebesar 1.
   }
 }
