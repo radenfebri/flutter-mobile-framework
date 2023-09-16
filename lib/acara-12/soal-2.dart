17685635be3d008af307c20990347a26fcfbb50e
Eka Febriansah <febriye12@gmail.com>
2023-09-16 07:31:10 +0700

    Update

--- lib/acara-12/soal-2.dart
+++ lib/acara-12/soal-2.dart
@@ -1,12 +1,21 @@
+// Fungsi delayedPrint akan mengembalikan Future yang menunda pencetakan pesan.
 Future delayedPrint(int seconds, String message) {
   final duration = Duration(seconds: seconds);
+
+  // Menunda eksekusi selama durasi yang telah ditentukan.
   return Future.delayed(duration).then((value) => message);
 }
 
 void main(List<String> args) {
+  // Mencetak pesan "Life".
   print("Life");
+
+  // Memanggil delayedPrint dengan penundaan 2 detik dan pesan "never flat".
   delayedPrint(2, "never flat").then((status) {
+    // Ketika Future selesai, mencetak pesan yang telah diambil dari delayedPrint.
     print(status);
   });
+
+  // Mencetak pesan "is".
   print("is");
 }
--- lib/acara-12/soal-3.dart
+++ lib/acara-12/soal-3.dart
@@ -1,27 +1,34 @@
 void main(List<String> args) async {
-  print("Ready. sing");
-  print(await line1());
-  print(await line2());
-  print(await line3());
-  print(await line4());
+  print("Ready. sing"); // Mencetak pesan "Ready. sing" ke layar.
+
+  // Menjalankan fungsi-fungsi berikut secara berurutan dan mencetak hasilnya saat tersedia.
+  print(await line1()); // Menunggu line1 selesai dan mencetak hasilnya.
+  print(await line2()); // Menunggu line2 selesai dan mencetak hasilnya.
+  print(await line3()); // Menunggu line3 selesai dan mencetak hasilnya.
+  print(await line4()); // Menunggu line4 selesai dan mencetak hasilnya.
 }
 
 Future<String> line1() async {
-  String grreeting = "Pernahkan kau merasa...";
-  return await Future.delayed(Duration(seconds: 5), () => (grreeting));
+  String greeting = "Pernahkan kau merasa..."; // Membuat pesan.
+  return await Future.delayed(Duration(seconds: 5),
+      () => greeting); // Mengembalikan pesan dengan penundaan 5 detik.
 }
 
 Future<String> line2() async {
-  String grreeting = "Pernahkan kau merasa...";
-  return await Future.delayed(Duration(seconds: 3), () => (grreeting));
+  String greeting = "Pernahkan kau merasa...";
+  return await Future.delayed(Duration(seconds: 3),
+      () => greeting); // Mengembalikan pesan dengan penundaan 3 detik.
 }
 
 Future<String> line3() async {
-  String grreeting = "Pernahkan kau merasa...";
-  return await Future.delayed(Duration(seconds: 2), () => (grreeting));
+  String greeting = "Pernahkan kau merasa...";
+  return await Future.delayed(Duration(seconds: 2),
+      () => greeting); // Mengembalikan pesan dengan penundaan 2 detik.
 }
 
 Future<String> line4() async {
-  String grreeting = "Hatimu hampa, pernahkah kau merasa hati mu kosong ............";
-  return await Future.delayed(Duration(seconds: 1), () => (grreeting));
+  String greeting =
+      "Hatimu hampa, pernahkah kau merasa hati mu kosong ............";
+  return await Future.delayed(Duration(seconds: 1),
+      () => greeting); // Mengembalikan pesan dengan penundaan 1 detik.
 }
